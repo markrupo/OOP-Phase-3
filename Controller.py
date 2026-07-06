5834d3bc95107b96850ece039d0864cf69e7cb2d
Mark <markrupo@hotmail.com>
2026-07-06 19:37:08 +0200

    Controller verkleinerung

--- Controller.py
+++ Controller.py
@@ -33,66 +33,54 @@ class controller:
             self.service.notenschnitt_status()[1]
         )
 
-    def belegungen_zeigen(self):
-        """Gibt Daten vom Service an die View weiter um bestandene Belegungen anzuzeigen"""
-        self.view.belegungen(
-            len(self.service.student.belegungen),
-            self.service.student.belegungen
-        ) 
-
-    def alle_module_zeigen(self):
-        """Gibt Daten vom Service an die View weiter um alle Module zu zeigen"""
-        self.view.print_alle_module(self.service.modul_liste())
-    
-    def notnenziel_aendern(self):
-        """Nimmt die Nutzereingabe entgegen und ändert das Notenziel"""
-        eingabe = input("0 = Zurück zum Dashboard\nneues ziel setzen:")
-        if eingabe == "0":
-            return self.dashboard_zeigen()
-        else:
-            return self.service.noten_ziel_aendern(eingabe)
-    
-    def zeitziel_aendern(self):
-        """Nimmt die Nutzereingabe entgegen und ändert das Zeitziel"""
-        eingabe = input("0 = Zurück zum Dashboard\nneues ziel setzen:")
-        if eingabe == "0":
-            return self.dashboard_zeigen()
-        return self.service.zeit_ziel_aendern(eingabe)
-    
-    def modul_belegen(self):
-        """Nimmt die Nutzereingabe entgegen und erstellt ein Belegung Objekt"""
-        modul_index = input("0 = Zurück zum Dashboard\nModul index aus der modul liste eingeben:")
-        if modul_index == "0":
-            return self.dashboard_zeigen()
-        os.system('cls' if os.name == 'nt' else 'clear')
-        note = input("0 = Zurück zum Dashboard\nNote eingeben:")
-        if note == "0":
-            return self.dashboard_zeigen()
-        os.system('cls' if os.name == 'nt' else 'clear')
-        pruefungstyp = input("0 = Zurück zum Dashboard\n1 = Klausur\n2 = Workbook\n3 = Portfolio\n4 = Präsentation\nZahl eingeben:")
-        if pruefungstyp == "0":
-            return self.dashboard_zeigen()
-        os.system('cls' if os.name == 'nt' else 'clear')
-        return self.service.pruefung_belegen(modul_index, note, pruefungstyp)
 
     def nutzer_eingabe(self):
         """Erlaubt Nutzern die Interaktion mit dem Programm"""
         eingabe = input(f"{"\n" * 2}1 = Dashboard\n2 = Belegungen\n3 = Module zeigen\n4 = Modul belegen\n5 = Notenziel ändern\n6 = Zeitziel ändern\nzahl eingeben:")
         os.system('cls' if os.name == 'nt' else 'clear')
-        if eingabe == "0":
-            return
+        
         if eingabe == "1":
             self.dashboard_zeigen()
+
         elif eingabe == "2":
-            self.belegungen_zeigen()
+            """Gibt Daten vom Service an View weiter um bestandene Belegungen anzuzeigen"""
+            self.view.belegungen(len(self.service.student.belegungen),self.service.student.belegungen)
+
         elif eingabe == "3":
-            self.alle_module_zeigen()
+            """Gibt Daten vom Service an View weiter um alle Module zu zeigen"""
+            self.view.print_alle_module(self.service.modul_liste())
+
         elif eingabe == "4":
-            print(self.modul_belegen())
+            """Nimmt die Nutzereingabe entgegen und erstellt ein Belegung Objekt"""
+            modul_index = input("0 = Zurück zum Dashboard\nModul index aus der modul liste eingeben:")
+            if modul_index == "0":
+                return self.dashboard_zeigen()
+            os.system('cls' if os.name == 'nt' else 'clear')
+            note = input("0 = Zurück zum Dashboard\nNote eingeben:")
+            if note == "0":
+                return self.dashboard_zeigen()
+            os.system('cls' if os.name == 'nt' else 'clear')
+            pruefungstyp = input("0 = Zurück zum Dashboard\n1 = Klausur\n2 = Workbook\n3 = Portfolio\n4 = Präsentation\nZahl eingeben:")
+            if pruefungstyp == "0":
+                return self.dashboard_zeigen()
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(self.service.pruefung_belegen(modul_index, note, pruefungstyp))
+
         elif eingabe == "5":
-           print(self.notnenziel_aendern())
+            """Nimmt die Nutzereingabe entgegen und ändert das Notenziel"""
+            eingabe = input("0 = Zurück zum Dashboard\nneues ziel setzen:")
+            if eingabe == "0":
+                return self.dashboard_zeigen()
+            else:
+                print(self.service.noten_ziel_aendern(eingabe))
+
         elif eingabe == "6":
-            print(self.zeitziel_aendern())
+            """Nimmt die Nutzereingabe entgegen und ändert das Zeitziel"""
+            eingabe = input("0 = Zurück zum Dashboard\nneues ziel setzen:")
+            if eingabe == "0":
+                return self.dashboard_zeigen()
+            else:
+                print(self.service.zeit_ziel_aendern(eingabe))
 
     def run(self):
         """Startet das Programm"""
